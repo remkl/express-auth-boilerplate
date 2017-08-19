56d20e255fed2331a8541d5d3d2b2a4c478b7f7f
Remi Kellett-Liew <remi.kl@gmail.com>
2017-08-19 18:12:13 +1000

    init repo

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+	email: { type: String, unique: true, lowercase: true },
+	password: String
+})
+
+userSchema.pre('save', function(next) {
+	const user = this
+
+	bcrypt.genSalt(10, (err, salt) => {
+		if (err) return next(err)
+
+		bcrypt.hash(user.password, salt, null, (err, hash) => {
+			if (err) return next(err)
+			user.password = hash
+			next()
+		})
+	})
+})
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+	bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+		if (err) callback(err)
+		callback(null, isMatch)
+	})
+}
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
